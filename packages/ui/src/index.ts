728f9dcfec2cc944add6d3229c0f4cf33d7b2d51
Maksim <kodermax@gmail.com>
2025-11-24 10:51:51 +0300

    Initial commit (history cleaned)

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,16 @@
+import { cx } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+export const cn = (...inputs: Parameters<typeof cx>) => twMerge(cx(inputs));
+
+// Export all components
+export * from "./button";
+export * from "./card";
+export * from "./dropdown-menu";
+export * from "./field";
+export * from "./input";
+export * from "./label";
+export * from "./separator";
+export * from "./textarea";
+export * from "./theme";
+export * from "./toast";
