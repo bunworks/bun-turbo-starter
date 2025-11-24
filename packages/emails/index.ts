ddf261aa6c1beab20458e001a53e9a68763f4e71
Maksim <kodermax@gmail.com>
2025-11-24 22:54:31 +0300

    up

--- packages/emails/index.ts
+++ packages/emails/index.ts
@@ -1,17 +1,6 @@
 import OtpSignInEmail from "./emails/otp-sign-in";
 import WelcomeEmail from "./emails/welcome";
-import WorkspaceInviteEmail from "./emails/workspace-invite";
-import ScriptReportEmail from "./emails/script-report";
-import EmailChangeVerification from "./emails/email-change-verification";
-import AgentMessageEmail from "./emails/agent-message";
 
-export {
-  OtpSignInEmail,
-  WelcomeEmail,
-  WorkspaceInviteEmail,
-  ScriptReportEmail,
-  EmailChangeVerification,
-  AgentMessageEmail,
-};
+export { OtpSignInEmail, WelcomeEmail };
 
 export { sendEmail, sendEmailHtml } from "./send";
